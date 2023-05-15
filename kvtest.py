902feb615f6bea9fa1a09a98f8f20c5a3048e8bf
TERANX <131871583+TERANX@users.noreply.github.com>
2023-05-15 20:05:17 +0300

    Добавил обработку исключений

--- kvtest.py
+++ kvtest.py
@@ -1,6 +1,10 @@
-a = int(input('Введите a: '))
-b = int(input('Введите b: '))
-c = int(input('Введите c: '))
+baddata = True
+while baddata == True:
+    try:
+         a = int(input('Введите a: '))
+         b = int(input('Введите b: '))
+         c = int(input('Введите c: '))
+         baddata = False
+    except:
+        print('Не удалось получить данные!')
 
-print(type(a))
-print(a+b)
