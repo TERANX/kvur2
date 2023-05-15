0e2f943be956ad33b0bd1746d80ae6a88d2df09e
TERANX <131871583+TERANX@users.noreply.github.com>
2023-05-15 20:46:03 +0300

    Изменил импорт sqrt

--- kvtest.py
+++ kvtest.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 baddata = True
 while baddata == True:
     try:
@@ -12,12 +12,12 @@ while baddata == True:
 D = (b * b) - (4 * a * c)
 
 if D > 0:
-    d = math.sqrt(D)
+    d = sqrt(D)
     x1 = ((-b) + d) / (2 * a)
     x2 = ((-b) - d) / (2 * a)
-    printf(f'Уравнение имеет два корня x1 = {x1}, x2 = {x2}.')
+    print(f'Уравнение имеет два корня x1 = {x1}, x2 = {x2}.')
 elif D == 0:
     x1 = (-b) / (2 * a)
-    printf(f'Уравнение имеет один корень x1 = {x1}.')
+    print(f'Уравнение имеет один корень x1 = {x1}.')
 else:
     print('Уравнение не имеет корней')
